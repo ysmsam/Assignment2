09fd1048f44d5400b111580ec551791d3d2047d8
SHENG MING YAN <ysmsam@hotmail.com>
2022-03-18 00:16:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment2
+ database assignment 2
---
+++ assignment 2.sql
@@ -0,0 +1,70 @@
+DECLARE
+	CURSOR c_transaction IS
+		SELECT *
+			FROM new_transactions;
+	
+	CURSOR c_account IS
+		SELECT account_no, account_type_code, account_balance
+			FROM account
+			FOR UPDATE;
+			
+			v_account_no 	account.account_no%TYPE;
+	
+	CURSOR c_history IS
+		SELECT transaction_no, transaction_date, description
+			FROM transaction_history;
+			
+			v_transaction_no 	transaction_history.transaction_no%TYPE;
+			v_transaction_date 	transaction_history.transaction_date%TYPE;
+			v_transaction_description 	transaction_history.description%TYPE;
+			
+	CURSOR c_detail IS
+		SELECT account_no, transaction_no, transaction_type, transaction_amount
+			FROM transaction_detail
+			WHERE transaction_no = v_transaction_no AND account_no = v_account_no;
+
+			
+			v_transaction_type 	transaction_detail.transaction_type%TYPE;
+			v_transaction_amount 	transaction_detail.transaction_amount%TYPE;
+	
+
+BEGIN
+
+	FOR r_transactions IN c_transactions LOOP
+		v_account_no := r_transactions.account_no;
+		-- v_transaction_no := r_transactions.transaction_no;
+		v_transaction_date := r_transactions.transaction_date;
+		-- v_transaction_type := r_transactions.transaction_type;
+		-- v_transaction_description := r_transactions.description;
+		v_transaction_amount := r_transactions.transaction_amount;
+		
+		SELECT DISTINCT transaction_no, transaction_date, description
+			INTO v_transaction_no, v_transaction_date, v_transaction_description
+			FROM new_transactions
+			WHERE v_transaction_no = r_transactions.transaction_no;
+		
+		FOR r_history IN c_history LOOP
+			
+			-- insert data into history TABLE
+			INSERT INTO transaction_history
+			VALUES (v_transaction_no, v_transaction_date, v_transaction_description);
+			
+			
+			FOR r_detail IN c_detail LOOP
+				
+				-- insert data into detail TABLE
+				INSERT INTO transaction_detail
+				VALUES (v_account_no, v_transaction_no, v_transaction_type, v_transaction_amount);
+			
+				
+				
+			END LOOP;
+		END LOOP;
+		
+		-- delete the row in new_transactions
+		
+	END LOOP;
+
+
+END;
+/
