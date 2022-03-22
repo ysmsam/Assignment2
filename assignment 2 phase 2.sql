41274f11cb25ebe72c88a907cd7a3ab5a1fb9815
SHENG MING YAN <ysmsam@hotmail.com>
2022-03-21 23:43:29 -0600

    assignment 2 phase 2
    assignment 2 phase 2

--- assignment 2 phase 2.sql
+++ assignment 2 phase 2.sql
@@ -6,24 +6,15 @@ DECLARE
 			v_transaction_no 	 NUMBER;
 			v_transaction_date  	DATE;
 			v_transaction_description 	 VARCHAR2(100);
-			-- v_transaction_type  	CHAR(1);
-			-- v_transaction_amount   	NUMBER;
 			
 	CURSOR c_transactions_2 IS
 		SELECT *
 			FROM new_transactions
 			WHERE transaction_no = v_transaction_no;
-	
-	-- CURSOR c_account IS
-	-- 	SELECT account_no, account_type_code, account_balance
-	-- 		FROM account
-	-- 		FOR UPDATE;
 			
 			v_account_no 	 NUMBER;
 			v_account_balance  	NUMBER;
-			-- v_account_type_code 	account.account_type_code%TYPE;
-			
-			-- v_account_trans_type 	account_type.default_trans_type%TYPE;
+
 			v_account_trans_type 	CHAR(1);
 			v_account_type_code 	VARCHAR2(2);
 			
@@ -33,47 +24,62 @@ DECLARE
 			v_transaction_description_2  	VARCHAR2(100);
 			v_transaction_type_2 	 CHAR(1);
 			v_transaction_amount_2  	NUMBER;
+			
+			v_error_msg 	wkis_error_log.error_msg%TYPE;
+			ex_nodatafound_1 	EXCEPTION;
+			ex_invaid_1 	EXCEPTION;
+			ex_invaid_2 	EXCEPTION;
+			
+			v_debit_value 	NUMBER;
+			v_credit_value 	NUMBER;
+			
+			k_transaction_type_credit 	CONSTANT CHAR(1) := 'C';
+			k_transaction_type_debit 	CONSTANT CHAR(1) := 'D';
 	
 			
 BEGIN
 
 	FOR r_transactions IN c_transactions LOOP
 		BEGIN
-			-- v_account_no := r_transactions.account_no;
+
 			v_transaction_no := r_transactions.transaction_no;
+			IF SQL%NOTFOUND THEN
+				RAISE ex_nodatafound_1;
+			END IF;
+			
 			v_transaction_date := r_transactions.transaction_date;
-			-- v_transaction_type := r_transactions.transaction_type;
 			v_transaction_description := r_transactions.description;
-			-- v_transaction_amount := r_transactions.transaction_amount;
-			
-			-- SELECT DISTINCT transaction_no, transaction_date, description
-			-- 	INTO v_transaction_no, v_transaction_date, v_transaction_description
-			-- 	FROM new_transactions
-			-- 	WHERE transaction_no = r_transactions.transaction_no;
-			
-			
 				
 			-- insert data into history TABLE
 			INSERT INTO transaction_history
 			VALUES (v_transaction_no, v_transaction_date, v_transaction_description);
 
+				FOR r_transactions_2 IN c_transactions_2 LOOP					
+					
+					v_transaction_no_2 := r_transactions_2.transaction_no;
+					-- Missing transaction number (NULL transaction number)
+					IF SQL%NOTFOUND THEN
+						RAISE ex_nodatafound_1;
+					END IF;
+					
+					v_account_no_2 := r_transactions_2.account_no;
+					-- Invalid account number
 			
-			-- FOR r_account IN c_account LOOP
-				
-			-- 	v_account_balance := r_account.account_balance;
-			-- 	SELECT default_trans_type
-			-- 		INTO v_account_trans_type
-			-- 		FROM account_type
-			-- 		WHERE r_account.account_type_code = account_type.account_type_code;
-				
-				FOR r_transactions_2 IN c_transactions_2 LOOP
-				
-				v_account_no_2 := r_transactions_2.account_no;
-				v_transaction_no_2 := r_transactions_2.transaction_no;
-				-- v_transaction_date_2 := r_transactions_2.transaction_date;
-				v_transaction_type_2 := r_transactions_2.transaction_type;
-				-- v_transaction_description_2 := r_transactions_2.description;
-				v_transaction_amount_2 := r_transactions_2.transaction_amount;
+					
+					IF r_transactions_2.transaction_amount < 0 THEN
+						RAISE ex_invaid_1;
+					ELSE
+						v_transaction_amount_2 := r_transactions_2.transaction_amount;
+					END IF;
+					
+					
+					-- Invalid transaction type & initialize v_transaction_type_2
+					IF r_transactions_2.transaction_type != k_transaction_type_credit OR r_transactions_2.transaction_type != k_transaction_type_debit THEN
+						RAISE ex_invaid_2;
+					ELSE
+						v_transaction_type_2 := r_transactions_2.transaction_type;
+					END IF;
+					
 					
 					-- insert data into detail TABLE
 					INSERT INTO transaction_detail
@@ -111,7 +117,26 @@ BEGIN
 			-- delete the row in new_transactions
 			DELETE FROM new_transactions WHERE transaction_no = r_transactions.transaction_no;
 			
+			
+			--COMMIT;
+			
 			-- EXCEPTION
+			EXCEPTION
+				WHEN ex_nodatafound_1 THEN
+					
+					v_error_msg := SUBSTR(SQLERRM, 1, 100);
+					
+					INSERT INTO wkis_error_log
+					(error_msg)
+					VALUES(v_error_msg);
+					
+					--COMMIT;
+					
+				WHEN ex_invaid_1 THEN
+					DBMS_OUTPUT.PUT_LINE('Negative Value given for a transaction amount');
+				
+				WHEN ex_invaid_2 THEN
+					DBMS_OUTPUT.PUT_LINE('Invalid transaction type');
 			
 			
 		END;
